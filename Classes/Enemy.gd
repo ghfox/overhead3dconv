ab037ed65e0571338e34d401609b840472980305
Hal <hwest.aycock@gmail.com>
2022-01-09 00:17:24 -0600

    rat looks and spots

--- Classes/Enemy.gd
+++ Classes/Enemy.gd
@@ -19,7 +19,51 @@ var speed = 0
 var targetLoc = null
 var flankRight = true
 
+var idle = funcref(self,'meander')
+var alerted = funcref(self,'hunt')
+var spotted = funcref(self,'charge')
+var charged = funcref(self,'withdraw')
+
 #Alerts, spots, and being attacked forces state options
+
+func _process(delta):
+	if(!isAlive):
+		return
+	if isAlerted():
+		alerted.call_funcv([delta])
+	if isSpotted():
+		if isCharged(spottedLoc):
+			charged.call_funcv([delta])
+			return
+		spotted.call_funcv([delta])
+	if isIdle():
+		idle.call_funcv([delta])
+		return
+
+func _physics_process(delta):
+	if(!isAlive):
+		return
+	var space_state = get_world().direct_space_state
+	look(space_state)
+
+#must be invoked by physics thread
+#Adds the nearest ETarget that enemy has a 180 degree line of sight on
+func look(space_state):
+	var closest = 99999
+	var current = null
+	var tempDist
+	for dude in get_tree().get_nodes_in_group("ETargets"):
+		var ray = space_state.intersect_ray(translation,dude.translation,[self])
+		if(ray.collider_id == dude.get_instance_id()):
+			tempDist = translation.distance_to(dude.translation)
+			if(Vector3(1,0,0).dot(to_local(dude.translation)) > 0):
+				if(tempDist < closest):
+					current = dude
+					closest = tempDist
+	if(current != null):
+		spottedLoc = current.translation
+	else:
+		spottedLoc = null
 
 func pulse():
 	pass
@@ -30,8 +74,11 @@ func isIdle():
 func isAlerted():
 	return (alertLoc != null)
 
-func isSpotting():
+func isSpotted():
 	return (spottedLoc != null)
+
+func isCharged(loc):
+	return translation.distance_to(loc) < proximity
 
 func hit(dam):
 	health -= dam
@@ -45,54 +92,59 @@ func death():
 func wait():
 	speed = 0
 
-func meander():
+func meander(delta):
 	if(randi()%2==1):
 		speed = walk
 	else:
 		speed = 0 
 
-func patrol():
+func patrol(delta):
 	pass
 
-func hunt():
+func hunt(delta):
 	speed = run
 	targetLoc = alertLoc
 
-func approach():
+func approach(delta):
 	speed = walk
 	targetLoc = alertLoc
 
-func flee():
+func flee(delta):
 	pass
 
-func charge():
+func charge(delta):
 	speed = run
 	targetLoc = spottedLoc
+	turnTowards(targetLoc, PI/2 * delta)
 
-func maintain():
+func maintain(delta):
 	speed = 0
 	targetLoc = spottedLoc
 
-func circle():
+func circle(delta):
 	flankRight = randi()%2==1
 	if(circleToLunge):
 		speed = run
 	else:
 		speed = walk
 
-func lunge():
+func lunge(delta):
 	pass
 
-func withdraw():
+func withdraw(delta):
 	pass
 
 func turnTowards(targetVec, delta):
 	var final = (targetVec - translation)
-	var temp = lerp_angle(rotation.y, atan2(-final.z,final.x), 1 * delta) - rotation.y
+	var temp = turnby(rotation.y, atan2(-final.z,final.x), PI * delta) - rotation.y
 	rotate_y(temp)
 
-func lerp_angle(from, to, weight):
-	return from + short_angle_dist(from, to) * weight
+func turnby(from, to, weight):
+	var short = short_angle_dist(from, to)
+	if abs(short) > weight:
+		return from + (weight * sign(short))
+	else:
+		return from + short
 
 func short_angle_dist(from, to):
 	var max_angle = PI * 2
--- Scn/Creatures/Rat_small.gd
+++ Scn/Creatures/Rat_small.gd
@@ -1,7 +1,4 @@
 extends Enemy
 
-func _process(delta):
-	turnTowards(Inventory.player.translation, delta)
-
 func _ready():
 	pass # Replace with function body.
